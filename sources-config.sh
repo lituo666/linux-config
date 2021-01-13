909b6b6c1c5d93e64d33552816e53ccf00cc8324
lituo666 <751634696@qq.com>
2021-01-13 08:38:23 +0800

    add sources-config.sh.

---
+++ sources-config.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "备份源文件sources.list"
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+echo "修改源地址"
+sudo bash -c "cat > /etc/apt/sources.list" << EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+EOF
+
+echo "更新源"
+sudo apt-get update
+
+echo "安装常用软件"
+sudo apt-get install vim zsh git
